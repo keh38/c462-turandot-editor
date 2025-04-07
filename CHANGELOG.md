9459b58c9f0b3d0a825524805f5542c0181bfe19
keh38 <Ken_Hancock@meei.harvard.edu>
2025-04-07 13:50:42 -0400

    built v2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,13 @@
 ## Changelog
 
-### 2.0.1 (unreleased)
+### 2.1 (2025-04-07)
 #### Fixed
 - install location/registry setting
 - clear property grids when no object is selected
 #### Changed
 - automatically append "Turandot" prefix to .xml files
+#### Added
+- parameter slider specification
 
 ---
 
--- Turandot Editor/Properties/AssemblyInfo.cs
+++ Turandot Editor/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Turandot Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MEEI")]
+[assembly: AssemblyCompany("EPL")]
 [assembly: AssemblyProduct("Turandot Editor")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyCopyright("Copyright © Eaton-Peabody Labs 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
